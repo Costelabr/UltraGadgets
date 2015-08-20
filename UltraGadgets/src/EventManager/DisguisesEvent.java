0f9e6473a26c31ce93d7b16da3a4d1bc25a1e663
Floodeer <Floodeer@Floodeer>
2015-08-20 14:56:36 -0300

    Versão 2.0.2

---
+++ UltraGadgets/src/EventManager/DisguisesEvent.java
@@ -0,0 +1,131 @@
+package EventManager;
+
+import org.bukkit.Bukkit;
+import org.bukkit.Color;
+import org.bukkit.FireworkEffect;
+import org.bukkit.Location;
+import org.bukkit.Material;
+import org.bukkit.Sound;
+import org.bukkit.entity.Entity;
+import org.bukkit.entity.Player;
+import org.bukkit.entity.WitherSkull;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.block.Action;
+import org.bukkit.event.entity.EntityDamageByEntityEvent;
+import org.bukkit.event.entity.EntityExplodeEvent;
+import org.bukkit.event.player.PlayerInteractEvent;
+import org.bukkit.event.player.PlayerToggleFlightEvent;
+import org.bukkit.event.player.PlayerToggleSneakEvent;
+import org.bukkit.metadata.FixedMetadataValue;
+import org.bukkit.util.Vector;
+
+import Core.FireworkNMSHandler;
+import Core.ParticleEffect;
+import Core.UtilCooldown;
+import Core.UtilParticle;
+import Core.UtilParticle.ParticleType;
+import Core.UtilVelocity;
+import Menus.DisguiseMenu;
+
+import com.floodeer.gadgets.UltraGadgets;
+
+public class DisguisesEvent extends FireworkNMSHandler implements Listener{
+	 
+	UltraGadgets plugin = UltraGadgets.getMain();
+	
+	@EventHandler
+	public void onPlayerClick(PlayerInteractEvent e) {
+		if(e.getAction() != Action.LEFT_CLICK_AIR) {
+			return;
+		}
+		Player p = (Player)e.getPlayer();
+		if(!plugin.getDisguiseMenu().hasDisguise(p)) return;
+		if(DisguiseMenu.disguiseType.get(p.getUniqueId()) == "Enderman" && 
+		p.hasPermission("enderman.teleport")) {
+			 
+		 Location l = plugin.getUtilLocation().getTargetBlock(p, 50);
+		 if(l.getBlock().getType() != Material.AIR) {
+			 p.teleport(l.add(0, 1, 0));
+			 p.playSound(p.getLocation(), Sound.ENDERMAN_TELEPORT, 1, 1);
+		 }
+	   }
+		if(DisguiseMenu.disguiseType.get(p.getUniqueId()) == "WitherSkeleton") {
+			if(UtilCooldown.tryCooldown(p, "WSkull", 2000)) {
+			WitherSkull w = p.launchProjectile(WitherSkull.class);
+			w.setMetadata("SkullLaunch", new FixedMetadataValue(plugin, null));
+			w.setIsIncendiary(false);
+		}else{
+			p.sendMessage(plugin.getMessagesFile().otherCooldown);
+		}
+	 }
+	
+	}
+	
+	@EventHandler
+	public void toggle(PlayerToggleSneakEvent e) {
+		Player p = (Player)e.getPlayer();
+		if(!p.isSneaking()) {
+		if(DisguiseMenu.disguiseType.get(p.getUniqueId()) == "Creeper") {
+			if(UtilCooldown.tryCooldown(p, "Creeper", 5000)) {
+			ParticleEffect.EXPLOSION_HUGE.display(0, 0, 0, 1, 150, p.getLocation(), 50.0D);
+            p.playSound(p.getLocation(), Sound.EXPLODE, 5, 1);
+			  for (Entity entity : p.getNearbyEntities(8, 2, 8)) {
+			         if (entity instanceof Player) {
+			             final Player nearbyPlayer = (Player)entity;
+			             nearbyPlayer.setVelocity(p.getLocation().getDirection().multiply(0.3).add(new Vector(0.0, 1.2, 0.0)));
+			             nearbyPlayer.playSound(p.getLocation(), Sound.EXPLODE, 5, 1);
+
+			         }
+			     }
+			}else{
+				p.sendMessage(plugin.getMessagesFile().otherCooldown);
+		}
+	  }
+   }
+}
+	
+	@EventHandler
+	public void update(UpdateEvent e) {
+	  if(e.getType() == UpdateType.TICK) {
+		for(Player player : Bukkit.getOnlinePlayers()) {
+			if(DisguiseMenu.disguiseType.get(player.getUniqueId()) == "Blaze") {
+			  if (player.isSneaking()) {
+			      player.leaveVehicle();
+			        player.eject(); 
+			          new UtilParticle(ParticleType.FLAME, 0.05000000074505806D, 2, 0.30000001192092896D).sendToLocation(player.getLocation().add(0.0D, 0.5D, 0.0D));
+			           new UtilParticle(ParticleType.SMOKE_LARGE, 0.05000000074505806D, 2, 0.30000001192092896D).sendToLocation(player.getLocation().add(0.0D, 0.20000000298023224D, 0.0D));
+			            UtilVelocity.velocity(player, 0.8D, 0.1D, 1.0D, true);
+			            player.setAllowFlight(true);
+			   }
+		  }
+	  }
+	}
+} 
+	
+	@EventHandler
+	public void explode(final EntityExplodeEvent e) {
+		if(e.getEntity() instanceof WitherSkull) {
+			if(e.getEntity().hasMetadata("SkullLaunch")) {
+				e.setCancelled(true);
+				 playFirework(e.getEntity().getLocation(), FireworkEffect.builder().withColor(Color.BLACK).withColor(Color.WHITE).build());
+			}
+		}
+	}
+	
+	@EventHandler
+	public void damage(EntityDamageByEntityEvent e) {
+		if(e.getDamager() instanceof WitherSkull) {
+			if(e.getDamager().hasMetadata("SkullLaunch")) {
+				e.setCancelled(true);
+			}
+		}
+	}
+	
+	@EventHandler
+	public void updateFly(PlayerToggleFlightEvent e) {
+		if(DisguiseMenu.disguiseType.get(e.getPlayer()) == "Blaze") {
+			e.setCancelled(true);
+		}
+	}
+}
---
+++ UltraGadgets/src/Gadgets/ExplosiveSheep.java
@@ -0,0 +1,143 @@
+package Gadgets;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+import org.bukkit.Bukkit;
+import org.bukkit.DyeColor;
+import org.bukkit.Location;
+import org.bukkit.Material;
+import org.bukkit.Sound;
+import org.bukkit.craftbukkit.v1_8_R3.entity.CraftSheep;
+import org.bukkit.entity.Player;
+import org.bukkit.entity.Sheep;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
+import org.bukkit.event.Listener;
+import org.bukkit.event.block.Action;
+import org.bukkit.event.entity.EntityChangeBlockEvent;
+import org.bukkit.event.entity.EntityDamageEvent;
+import org.bukkit.event.entity.SheepRegrowWoolEvent;
+import org.bukkit.event.player.PlayerInteractEvent;
+import org.bukkit.event.player.PlayerShearEntityEvent;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.potion.PotionEffect;
+import org.bukkit.potion.PotionEffectType;
+
+
+import Core.ParticleEffect;
+import Core.Util18;
+import Core.UtilCooldown;
+import EventManager.UpdateEvent;
+import EventManager.UpdateType;
+
+import com.floodeer.gadgets.UltraGadgets;
+
+public class ExplosiveSheep implements Listener {
+	
+	UltraGadgets plugin = UltraGadgets.getMain();
+	List<Sheep> sheep = new ArrayList<Sheep>();
+	
+	public int random(Integer[] integers) {
+		Random rand = new Random();
+		return integers[rand.nextInt(integers.length)];
+	}
+	
+	@SuppressWarnings("deprecation")
+	@EventHandler
+	public void onUpdate(UpdateEvent e) {
+		if(e.getType() == UpdateType.TICKS_2) {
+			int color = random(new Integer[] {1,3,7,9,5,4,6});
+			for(Sheep s : sheep) {
+				s.setColor(DyeColor.getByData((byte)color));
+				s.getLocation().getWorld().playSound(s.getLocation(), Sound.SHEEP_IDLE, 5, -25);
+			}
+		}
+	}
+	
+	public void createSheep(final Location l) {
+		final Sheep s = l.getWorld().spawn(l, Sheep.class);
+		sheep.add(s);
+		((CraftSheep)s).getHandle().move(0, 0, 0);
+		s.addPotionEffect(new PotionEffect(PotionEffectType.SLOW, 100, 100, true));
+		s.setCustomNameVisible(false);
+		Bukkit.getScheduler().scheduleSyncDelayedTask(plugin, new Runnable() {
+			
+			@Override
+			public void run() {
+				s.getLocation().getWorld().playSound(s.getLocation(), Sound.EXPLODE, 5, -3F);
+				ParticleEffect.EXPLOSION_NORMAL.display(0, 0, 0, 1, 200, s.getLocation(), 25);
+				sheep.remove(s);
+				s.remove();
+			}
+		}, 4*20);
+	}
+	
+	
+	 @EventHandler
+	  public void paramPlayerUseExplosiveSheep(PlayerInteractEvent paramPlayerUseExplosiveSHeepEvent)
+	  {
+	    final Player paramPlayer = paramPlayerUseExplosiveSHeepEvent.getPlayer();
+	    Action paramAction = paramPlayerUseExplosiveSHeepEvent.getAction();
+	    if ((paramAction != Action.RIGHT_CLICK_AIR) && (paramAction != Action.RIGHT_CLICK_BLOCK)) {
+	      return;
+	    }
+	    ItemStack paramItem = paramPlayer.getItemInHand();
+	    if (this.plugin.getUtilBlock().usable(paramPlayerUseExplosiveSHeepEvent.getClickedBlock())) {
+	      return;
+	    }
+	    if (this.plugin.getItem().isGadgetItem(paramItem, plugin.getMessagesFile().ExplosiveSheepName)) {
+	      if(UtilCooldown.tryCooldown(paramPlayer, "ExplosiveSheep", plugin.getConfigFile().explosiveSheepCooldown)) {
+	        createSheep(paramPlayer.getLocation());
+	      }else{
+	    	  long cooldown = UtilCooldown.getCooldown(paramPlayer, "ExplosiveSheep") / 1000L;
+	          plugin.getMessagesFile().sendCooldownMessage(paramPlayer, "Explosive Sheep", "ExplosiveSheep", cooldown);
+	          paramPlayer.playSound(paramPlayer.getLocation(), Sound.valueOf(plugin.getConfig().getString("Som-Cooldown")), 1, 1);
+	          Util18.sendTitle(paramPlayer, 
+	          plugin.getMessagesFile().titleMessage,
+	          plugin.getMessagesFile().subTitleMessage.replaceAll("<COOLDOWN>", String.valueOf(cooldown)).replaceAll("<GADGET>", Tipos.getPlayerGadget.get(paramPlayer)), 
+	          plugin.getConfig().getInt("FadeIn-Title-Time"), plugin.getConfig().getInt("FadeStay-Title-Time"), plugin.getConfig().getInt("FadeOut-Title-Time"));
+	      }
+	    }
+	  }
+	 
+	 @EventHandler
+	 public void damn(EntityDamageEvent e) {
+		 if(e.getEntity() instanceof Sheep) {
+			 Sheep s = (Sheep)e.getEntity();
+			 if(sheep.contains(s)) {
+				 e.setCancelled(true);
+			 }
+		 }
+	 }
+	 
+	 @EventHandler(priority = EventPriority.MONITOR)
+	 public void entityIsSheep(SheepRegrowWoolEvent e) {
+		 if(e.getEntity() instanceof Sheep) {
+			 Sheep s = (Sheep)e.getEntity();
+			 if(sheep.contains(s)) {
+				 e.setCancelled(true);
+			 }
+		 }
+	 }
+	 
+	 @EventHandler(priority = EventPriority.MONITOR)
+	 public void entityIsSheep2(PlayerShearEntityEvent e) {
+		 if(e.getEntity() instanceof Sheep) {
+			 Sheep s = (Sheep)e.getEntity();
+			 if(sheep.contains(s)) {
+				 e.setCancelled(true);
+			 }
+		 }
+	 }
+	 
+	 @EventHandler(priority = EventPriority.MONITOR)
+	 public void entityChange(EntityChangeBlockEvent e) {
+		 if(e.getEntity() instanceof Sheep) {
+			 if((e.getTo() == Material.DIRT) && (e.getBlock().getType() == Material.GLASS)) {
+				 e.setCancelled(true);
+			 }
+		 }
+	 }
+}
